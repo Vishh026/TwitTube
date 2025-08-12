2034db4b790786908208e6342538443b20dc1487
unknown <vaishnavidhavan026@gmail.com>
2025-08-12 13:13:16 +0530

    Mongodb connected

---
+++ src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+
+const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`)
+        console.log(`Mongodb connected !! DB HOST: ${connectionInstance.connection.host}`);
+    
+    } catch (error) {
+        console.error("Mongodb Connection Failed!" ,error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
